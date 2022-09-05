e867018c32a57ec4204803bc735ab8d35f71e9f6
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-05 16:09:46 +0530

    Refactored : Adding Reverse of a number Program

---
+++ src/com/bridgelabz1/ReverseOfNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseOfNumber {
+
+	public static void main(String[] args) {
+		int reverse = 0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the number that you want to reverse : ");
+
+		int number = sc.nextInt();
+
+		while (number != 0) {
+			int remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+			number = number / 10;
+
+		}
+		System.out.println("The reverse of the given number is: " + reverse);
+
+	}
+}
