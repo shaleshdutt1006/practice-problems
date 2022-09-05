ecbb1f1031b3bcab3866fe37a5ecc2c882076743
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-05 16:14:13 +0530

    Refactored : Adding Strong Number check Program

---
+++ src/com/bridgelabz2/StrongNumber.java
@@ -0,0 +1,48 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+
+public class StrongNumber {
+
+    static int factorial(int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static boolean isStrongNumber(int number) {
+        int temp = number;
+        int digits, sum = 0;
+        boolean flag = false;
+
+        while (temp != 0) {
+            digits = temp % 10;
+            sum = sum + factorial(digits);
+            temp = temp / 10;
+
+        }
+        flag = sum == number;
+        return  flag;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number you want to check ");
+        int number = sc.nextInt();
+    if (isStrongNumber(number)){
+        System.out.println(number + " is a Strong Number");
+
+    }else {
+        System.out.println(number + " is not a Strong Number");
+    }
+
+
+
+    }
+}
