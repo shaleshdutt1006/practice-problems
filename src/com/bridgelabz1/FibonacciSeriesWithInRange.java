10c06349f5b3c9a4deca48bba94e659d282d711b
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-05 16:05:55 +0530

    Refactored : Adding Fibonacci Series with in a specified range program

--- src/com/bridgelabz1/FibonacciSeriesWithInRange.java
+++ src/com/bridgelabz1/FibonacciSeriesWithInRange.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
-public class FibonacciSeriesNthTerm {
+public class FibonacciSeriesWithInRange {
 
     public static void main(String[] args) {
-        int a = 0, b = 1, number, NextTerm = 0;
 
-        System.out.println("Please Enter which number you want to check ");
+
+        int a = 0, b = 1, number, NextTerm;
+
+        System.out.println("Please Enter which number you want to check");
 
         Scanner sc = new Scanner(System.in);
 
         number = sc.nextInt();
+        System.out.print(a + " , " + b);
 
         for (int i = 0; i <= number; i++) {
             NextTerm = a + b;
             a = b;
             b = NextTerm;
+            System.out.print(" , " + NextTerm);
 
         }
-        System.out.print(NextTerm);
 
     }
 
 }
+
 
