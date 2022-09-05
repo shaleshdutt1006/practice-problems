f318793decd9523506dc830c7391fdb5169ce721
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-05 16:13:05 +0530

    Refactored : Adding Perfect Square root checking Program

---
+++ src/com/bridgelabz2/PerfectSquare.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PerfectSquare {
+
+    static void squareRoot(int number){
+
+
+            if (Math.ceil(Math.sqrt(number)) == Math.floor(Math.sqrt(number)))
+            {
+                System.out.print(number + " is a Perfect Square Number");
+            }
+            else
+            {
+                System.out.print(number + " is not a Perfect Square Number");
+            }
+        }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number you want to check");
+        int number = sc.nextInt();
+
+        squareRoot(number);
+
+    }
+}
